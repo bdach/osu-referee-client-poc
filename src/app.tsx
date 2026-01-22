91fc10597667ddb3787fbb5f5ddcdc0bf93c2be9
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2026-01-22 21:43:19 +0100

    Electron + Webpack + React + Typescript setup via Electron Forge

---
+++ src/app.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.body);
+root.render(<h1>a quick brown fox jumps over the lazy dog</h1>)
